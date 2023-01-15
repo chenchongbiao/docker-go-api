0eaff426d0a2d33c249e119989023ae4813120dd
bluesky <379865549@qq.com>
2023-01-15 22:29:33 +0800

    添加容器初始化服务和，获取容器列表的功能

--- container/container_service.go
+++ container/container_service.go
@@ -1,8 +1,15 @@
 package container
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/godbus/dbus"
 	"github.com/linuxdeepin/go-lib/dbusutil"
-	"golang.org/x/vuln/client"
 )
 
 const (
@@ -11,7 +18,51 @@ const (
 	dbusInterface   = dbusServiceName
 )
 
-type Container struct {
+type ContainerService struct {
 	service *dbusutil.Service
 	cli     *client.Client
+}
+
+func (c *ContainerService) GetInterfaceName() string {
+	return dbusServiceName
+}
+
+func NewContainerService(service *dbusutil.Service, cli *client.Client) *ContainerService {
+	// 初始化日志
+	// 设置前缀
+	log.SetPrefix("ContainerService: ")
+	// 设置格式
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+
+	containerService := ContainerService{
+		service: service,
+		cli:     cli,
+	}
+	err := service.Export(dbusPath, &containerService)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = service.RequestName(dbusServiceName)
+	if err != nil {
+		log.Panic(err)
+	}
+	return &containerService
+}
+
+func (c *ContainerService) GetContainerList() (result string, busErr *dbus.Error) {
+	ctx := context.Background()
+	containers, err := c.cli.ContainerList(ctx, types.ContainerListOptions{All: true})
+	if err != nil {
+		log.Fatal("获取容器列表失败", err)
+		result = "获取容器列表失败"
+		return result, nil
+	}
+
+	// defer out.Close()
+	// io.Copy(os.Stdout, out)
+	list, _ := json.Marshal(containers)
+	result = string(list)
+	fmt.Println("容器列表获取成功")
+	return result, nil
 }
