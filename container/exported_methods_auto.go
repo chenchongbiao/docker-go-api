864d0063de404ed13f2d5874857a87c34fedbd88
bluesky <379865549@qq.com>
2023-01-15 22:31:06 +0800

    将获取容器列表的功能注册到sessionbus中

--- container/exported_methods_auto.go
+++ container/exported_methods_auto.go
@@ -1,1 +1,15 @@
 package container
+
+import (
+	"github.com/linuxdeepin/go-lib/dbusutil"
+)
+
+func (con *ContainerService) GetExportedMethods() dbusutil.ExportedMethods {
+	return dbusutil.ExportedMethods{
+		{
+			Name:    "GetContainerList",
+			Fn:      con.GetContainerList,
+			OutArgs: []string{"result"},
+		},
+	}
+}
