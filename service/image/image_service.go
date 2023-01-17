9a0817b08452dfcdd775a48211ba016d135dd0d9
bluesky <379865549@qq.com>
2023-01-17 17:51:23 +0800

    修改文件的位置

--- service/image/image_service.go
+++ service/image/image_service.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/bluesky/docker-go-api/adapter"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/godbus/dbus"
@@ -21,6 +22,7 @@ const (
 type ImageService struct {
 	service *dbusutil.Service
 	cli     *client.Client
+	adapter *adapter.ImageAdapter
 }
 
 func (image *ImageService) GetInterfaceName() string {
@@ -31,7 +33,11 @@ func NewImageService(service *dbusutil.Service, cli *client.Client) *ImageServic
 	imageService := ImageService{
 		service: service,
 		cli:     cli,
+		adapter: &adapter.ImageAdapter{
+			Cli: cli,
+		},
 	}
+
 	err := service.Export(dbusPath, &imageService)
 	if err != nil {
 		log.Println(err)
@@ -44,15 +50,21 @@ func NewImageService(service *dbusutil.Service, cli *client.Client) *ImageServic
 	return &imageService
 }
 
-func (image *ImageService) GetImageList() (result string, busErr *dbus.Error) {
-	ctx := context.Background()
-	images, err := image.cli.ImageList(ctx, types.ImageListOptions{})
+func (i *ImageService) GetImageList() (result string, busErr *dbus.Error) {
+	images, err := i.cli.ImageList(context.Background(), types.ImageListOptions{})
 	if err != nil {
 		log.Println("镜像列表获取失败", err)
 	}
 
+	// for _, image := range images {
+	// 	fmt.Printf("%#v \n\n", image)
+	// }
+
 	list, _ := json.Marshal(images)
 	result = string(list)
-	fmt.Println(result)
+
+	if 1 == 0 {
+		fmt.Printf(result)
+	}
 	return result, nil
 }
