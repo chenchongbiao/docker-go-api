ef389956caa02d0481d5e1773bd03ac6d59e5149
bluesky <379865549@qq.com>
2023-01-17 14:36:44 +0800

    完成镜像转换器 将api中获取的镜像数据重新构造

--- convertor/image.go
+++ convertor/image.go
@@ -1,38 +1,64 @@
 package convertor
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
-	"log"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
-func ImageConvert(imageSummary types.ImageSummary) string {
-	var dataMap map[string]interface{}
-	imgByte, err := json.Marshal(imageSummary)
-	if err != nil {
-		log.Println("镜像的ImageSummary类型转换byte数组失败")
-	}
+func ImageConvert(cli *client.Client, imageSummary types.ImageSummary, verbose bool) map[string]interface{} {
+	ctx := context.Background()
+	var imgMap map[string]interface{}        // 保存镜像的结构体转换的map
+	var imgInspectMap map[string]interface{} // 保存镜像
 
-	err = json.Unmarshal([]byte(imgByte), &dataMap)
-	if err != nil {
-		log.Println("镜像的byte数组类型转换Map失败")
-	}
-	fmt.Printf("字符串 %v\n", string(imgByte))
-	// 构造一个新的map
-	item := make(map[string]interface{})
+	imgByte, _ := json.Marshal(imageSummary)     // 镜像的ImageSummary类型转换byte数组
+	_ = json.Unmarshal([]byte(imgByte), &imgMap) // 镜像的byte数组类型转换Map
 
 	// 获取镜像id
-	id, _ := dataMap["Id"].(string)
+	id, _ := imgMap["Id"].(string)
+	// 构造一个新的map，存放最终结果
+	item := map[string]interface{}{
+		"id":          id[7:], // 切割字符串
+		"tags":        imgMap["RepoTags"],
+		"author":      imgMap["Author"],
+		"create_time": imgMap["Created"],
+		"size":        imgMap["Size"],
+	}
 
-	// tags, _ := dataMap["RepoTags"]
+	if verbose == true {
+		imgInspect, _, _ := cli.ImageInspectWithRaw(ctx, id)
+		imgInspectByte, _ := json.Marshal(imgInspect)              // 镜像的ImageInspect类型转换byte数组
+		_ = json.Unmarshal([]byte(imgInspectByte), &imgInspectMap) // 镜像的byte数组类型转换Map
+		// config := imgInspectMap["Config"]
+		config := imgInspectMap["Config"].(map[string]interface{})
+		if config["Cmd"] != nil {
+			// var cmd []string
+			// value := reflect.ValueOf(config["Cmd"])
+			// for i := 0; i < value.Len(); i++ {
+			// 	cmd = append(cmd, value.Index(i).Interface().(string))
+			// }
+			// fmt.Printf("%#v", cmd)
+			item["cmd"] = config["Cmd"]
+		}
 
-	// 切割字符串
-	item["Id"] = id[7:]
+		item["tty"] = config["Tty"]
+		item["open_stdin"] = config["OpenStdin"]
+		item["architecture"] = imgInspectMap["Architecture"]
+		item["os"] = imgInspectMap["Os"]
+		item["ports"] = PortConvert(config["ExposedPorts"].(map[string]interface{}))
 
-	itemByte, err := json.Marshal(item)
+	}
 
-	fmt.Println(string(itemByte))
-	return string(itemByte)
+	if 1 == 1 {
+		// fmt.Printf("镜像数据的json   %v\n", string(imgByte))
+
+	}
+	// fmt.Println(string(imgInspectByte))
+
+	// itemByte, err := json.Marshal(item)
+
+	// fmt.Println(string(itemByte))
+	return item
 }
