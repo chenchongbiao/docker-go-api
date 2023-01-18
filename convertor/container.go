671d9b16c141d609ed499a57e7273505d7e684eb
bluesky <379865549@qq.com>
2023-01-18 12:11:24 +0800

    添加容器数据的转换器

--- convertor/container.go
+++ convertor/container.go
@@ -29,8 +29,7 @@ func (c *ContainerConvertor) ContainerConvert(container types.Container, verbose
 
 	containerJson, _ := json.Marshal(container) // 将容器的结构体数据转换为json数据
 	json.NewDecoder(strings.NewReader(string(containerJson))).Decode(&conMap)
-	fmt.Printf("%#v\n", conMap)
-	// fmt.Printf("%#v\n", conMap["Image"])
+	// fmt.Printf("%#v\n", conMap)
 
 	// 根据Image获取镜像
 	filter := filters.NewArgs()
@@ -38,14 +37,44 @@ func (c *ContainerConvertor) ContainerConvert(container types.Container, verbose
 	image, _ := c.cli.ImageList(context.Background(), types.ImageListOptions{Filters: filter})
 
 	item := map[string]interface{}{
-		"id":    conMap["Id"],
-		"name":  conMap["Names"],
-		"image": c.imgConvertor.ImageConvert(image[0], false),
+		"id":          conMap["Id"],
+		"name":        conMap["Names"],
+		"image":       c.imgConvertor.ImageConvert(image[0], false),
+		"create_time": conMap["Created"],
+		"status":      conMap["Status"],
+		"state":       conMap["State"],
 	}
-	fmt.Printf("%#v\n", item)
-	if verbose == false {
-		var conInspectMap map[string]interface{} // 存放容器详细信息的map
-		fmt.Println(conInspectMap)
+
+	if verbose == true {
+		// var conInspectMap map[string]interface{} // 存放容器详细信息的map
+		netSetting := conMap["NetworkSettings"].(map[string]interface{})
+
+		// 构造一个列表存放Networks里的所有key值
+		netsList := make([]string, 0, len(netSetting))
+		for value := range netSetting["Networks"].(map[string]interface{}) {
+			netsList = append(netsList, value)
+		}
+		netsMap := netSetting["Networks"].(map[string]interface{})
+		nets := netsMap[netsList[0]].(map[string]interface{})
+
+		// config :=
+		// 将镜像传入专转换器获取数据
+		imgItem := c.imgConvertor.ImageConvert(image[0], true)
+
+		item["cmd"] = imgItem["cmd"]
+		item["open_stdin"] = imgItem["open_stdin"]
+		item["network"] = map[string]interface{}{
+			"ip":          nets["IPAddress"],
+			"prefix":      nets["IPPrefixLen"],
+			"gateway":     nets["Gateway"],
+			"mac_address": nets["MacAddress"],
+			// ports=PortMappingConvertor.from_docker(host_cfg['PortBindings']),
+			"ports": PortsMapConvert(conMap["Ports"].([]interface{})),
+		}
+		// mounts=MountsConvertor.from_docker(attrs['Mounts']),
+		item["mounts"] = MountsConvert(conMap["Mounts"].([]interface{}))
+		fmt.Printf("%#v", item)
+
 	}
-	return nil
+	return item
 }
