a94d2b1f7d0700a6b9e4a3eb43739003f2faae6d
bluesky <379865549@qq.com>
2023-01-18 16:32:55 +0800

    修改不传入client

--- convertor/volume.go
+++ convertor/volume.go
@@ -1,23 +1,27 @@
 package convertor
 
-import (
-	"fmt"
+// type VolumeConvertor struct {
+// 	cli *client.Client
+// }
 
-	"github.com/docker/docker/client"
-)
+// func NewVolumeConvert(cli *client.Client) *VolumeConvertor {
+// 	volumeConvertor := VolumeConvertor{
+// 		cli: cli,
+// 	}
+// 	return &volumeConvertor
+// }
 
-type VolumeConvertor struct {
-	cli *client.Client
-}
-
-func NewVolumeConvert(cli *client.Client) *VolumeConvertor {
-	volumeConvertor := VolumeConvertor{
-		cli: cli,
+func VolumeConvert(volume map[string]interface{}) map[string]interface{} {
+	item := map[string]interface{}{
+		"name":        volume["Name"],
+		"driver":      volume["Driver"],
+		"mount_point": volume["Mountpoint"],
+		"scope":       volume["Scope"],
+		"create_time": volume["CreatedAt"],
 	}
-	return &volumeConvertor
-}
 
-func (v *VolumeConvertor) VolumeConvert(volume map[string]interface{}) map[string]interface{} {
-	fmt.Printf("%#v", volume)
-	return nil
+	// if verboes == true {
+	// 	item["driver_opts"] = volume["Options"]
+	// }
+	return item
 }
