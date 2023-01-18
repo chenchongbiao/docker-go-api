79a28b33cca378805fa632e2d1af2544142c0273
bluesky <379865549@qq.com>
2023-01-18 12:12:19 +0800

    端口映射的转换器不调用client小先改为面向过程调用

--- convertor/ports_mapping.go
+++ convertor/ports_mapping.go
@@ -1,22 +1,18 @@
 package convertor
 
-import (
-	"github.com/docker/docker/client"
-)
+// type PortsMapConvertor struct {
+// 	cli *client.Client
+// }
 
-type PortsMapConvertor struct {
-	cli *client.Client
-}
-
-func NewPortsMapConvertor(cli *client.Client) *PortsMapConvertor {
-	portsMapConvertor := PortsMapConvertor{
-		cli: cli,
-	}
-	return &portsMapConvertor
-}
+// func NewPortsMapConvertor(cli *client.Client) *PortsMapConvertor {
+// 	portsMapConvertor := PortsMapConvertor{
+// 		cli: cli,
+// 	}
+// 	return &portsMapConvertor
+// }
 
 // 将端口映射的数据重新构造
-func (c *PortsMapConvertor) PortsMapConvert(portsMap []interface{}) []map[string]interface{} {
+func PortsMapConvert(portsMap []interface{}) []map[string]interface{} {
 	var result []map[string]interface{}
 	for _, value := range portsMap {
 		mapp := value.(map[string]interface{})
