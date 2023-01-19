72cdfd82e4f2a07641cf24f52ff40189b0c97c10
bluesky <379865549@qq.com>
2023-01-19 12:43:18 +0800

    单独挑出name和driver字段如果为nil则赋值空字符串

--- convertor/mounts.go
+++ convertor/mounts.go
@@ -16,16 +16,27 @@ func MountsConvert(mounts []interface{}) []map[string]interface{} {
 	for _, value := range mounts {
 		mount := value.(map[string]interface{})
 		// fmt.Printf("%#v\n", mount)
-		result = append(result, map[string]interface{}{
-			"rw":     mount["RW"],
-			"dest":   mount["Destination"],
-			"mode":   mount["Mode"],
-			"src":    mount["Source"],
-			"prop":   mount["Propagation"],
-			"type":   mount["Type"],
-			"name":   mount["Name"],
-			"driver": mount["Driver"],
-		})
+		item := map[string]interface{}{
+			"rw":   mount["RW"],
+			"dest": mount["Destination"],
+			"mode": mount["Mode"],
+			"src":  mount["Source"],
+			"prop": mount["Propagation"],
+			"type": mount["Type"],
+		}
+		// 这两个字段存在空值情况单独挑出来判断
+		if mount["Name"] == nil {
+			item["name"] = ""
+		} else {
+			item["name"] = mount["Name"]
+		}
+
+		if mount["driver"] == nil {
+			item["driver"] = ""
+		} else {
+			item["driver"] = mount["Driver"]
+		}
+		result = append(result, item)
 	}
 	return result
 }
