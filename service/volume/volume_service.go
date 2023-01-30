c67f9bd7e1fc39d757be4d2a983b6615e34b1726
bluesky <379865549@qq.com>
2023-01-30 15:48:36 +0800

    添加volume的服务 以及获取列表的功能

---
+++ service/volume/volume_service.go
@@ -0,0 +1,69 @@
+package volume
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/bluesky/docker-go-api/adapter"
+	"github.com/docker/docker/api/types/volume"
+	"github.com/docker/docker/client"
+	"github.com/godbus/dbus"
+	"github.com/linuxdeepin/go-lib/dbusutil"
+)
+
+const (
+	dbusPath        = "/com/bluesky/docker/Volume"
+	dbusServiceName = "com.bluesky.docker.Volume"
+	dbusInterface   = dbusServiceName
+)
+
+type VolumeService struct {
+	service *dbusutil.Service
+	cli     *client.Client
+	adapter *adapter.VolumeAdapter
+}
+
+func (v *VolumeService) GetInterfaceName() string {
+	return dbusServiceName
+}
+
+func NewVolumeService(service *dbusutil.Service, cli *client.Client) *VolumeService {
+	volumeService := VolumeService{
+		service: service,
+		cli:     cli,
+		adapter: adapter.NewVolumeAdapter(cli),
+	}
+	err := service.Export(dbusPath, &volumeService)
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = service.RequestName(dbusServiceName)
+	if err != nil {
+		log.Println(err)
+	}
+	return &volumeService
+}
+
+func (v *VolumeService) GetVolumeList() (result string, busErr *dbus.Error) {
+	volumes, err := v.cli.VolumeList(context.Background(), volume.ListOptions{})
+	if err != nil {
+		log.Println("存储列表获取失败", err)
+	}
+	log.Println("存储列表获取成功")
+
+	items := v.adapter.List(volumes)
+	resultMap := map[string]interface{}{
+		"status": true,
+		"data":   items,
+	}
+
+	// 将map转换为json数据
+	resultJson, _ := json.Marshal(resultMap)
+	result = string(resultJson)
+
+	// fmt.Printf("%#v\n", result)
+
+	return result, nil
+}
